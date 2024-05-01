a57e0cecbda8ff89cdd1aa7b08b56dee83ee865f
Konstantin Zhuravlev <113041604+constZhur@users.noreply.github.com>
2024-05-01 13:19:38 +0300

    Проект проинициализирован

---
+++ src/main/java/ru/mirea/infinitejourneysbackend/InfiniteJourneysBackendApplication.java
@@ -0,0 +1,13 @@
+package ru.mirea.infinitejourneysbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfiniteJourneysBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InfiniteJourneysBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/mirea/infinitejourneysbackend/InfiniteJourneysBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mirea.infinitejourneysbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfiniteJourneysBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
